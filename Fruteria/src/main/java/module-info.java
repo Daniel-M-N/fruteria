c2d95d6f27a3150362f2ec83b5774110c980b92b
Koan <70034019+ByKoan@users.noreply.github.com>
2025-05-12 09:29:50 +0200

    Delete Fruteria directory

--- Fruteria/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.fruteria {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.fruteria to javafx.fxml;
-    exports com.example.fruteria;
-}
